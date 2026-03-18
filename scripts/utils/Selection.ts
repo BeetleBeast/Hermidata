c999919a5dbfb2b502d0a757c68a4843a4126215
BeetleBeast <xkeleter2@gmail.com>
2026-03-18 16:12:06 +0100

    feat(rss): update RSS script path and implement new RSS page functionality

--- scripts/utils/Selection.ts
+++ scripts/utils/Selection.ts
@@ -1,4 +1,4 @@
-export function getElement<T extends HTMLDivElement>(selector: string, parent: Document | HTMLDivElement = document): T {
+export function getElement<T extends HTMLElement>(selector: string, parent: Document | HTMLElement = document): T {
     const el = parent.querySelector(selector);
     if (!el) {
         throw new Error(`Element not found: ${selector}`);
