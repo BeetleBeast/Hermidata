1f0786b0803493379bd8d6b8df12f7463c401b9e
BeetleBeast <78571657+BeetleBeast@users.noreply.github.com>
2026-06-12 18:46:45 +0200

    feat: add search functionality to filter RSS feeds and update CSS for filtered state

--- scripts/rssPage/controller.ts
+++ scripts/rssPage/controller.ts
@@ -12,6 +12,8 @@ export class RSSPageController {
 
     private readonly reloadData = getElement<HTMLButtonElement>("#reloadData");
 
+    private readonly search = getElement<HTMLButtonElement>("#search");
+
     constructor(allHermidata: Record<string, Hermidata>, settings: Settings) {
         this.feed = new feed(allHermidata, settings);
         this.bulk = new Bulk(allHermidata, settings);
@@ -35,8 +37,28 @@ export class RSSPageController {
     private setEventListener() {
         this.removeEventListener();
         this.reloadData!.addEventListener('click', () => this.reload());
+        this.search!.addEventListener('input', (e) => this.updateFeedList(e) );
     }
     private removeEventListener() {
         this.reloadData!.removeEventListener('click', () => this.reload());
+    }
+
+    private updateFeedList(e: Event) {
+        const value = (e.target as HTMLInputElement).value;
+        const AllHermidataContainer = document.querySelector<HTMLDivElement>('.all-entries-container');
+        if (!AllHermidataContainer) return;
+        const allFeeds = AllHermidataContainer?.querySelectorAll<HTMLDivElement>('.feed');
+
+        if (value == '') {
+            AllHermidataContainer?.classList.remove('filtered');
+            allFeeds?.forEach(feed => feed.classList.remove('filtered'));
+        }
+        for (const feed of allFeeds || []) {
+            if (feed.textContent?.toLowerCase().includes(value.toLowerCase())) {
+                feed.classList.remove('filtered');
+                continue;
+            }
+            feed.classList.add('filtered');
+        }
     }
 }
