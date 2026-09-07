165997ca2d3d970631adc424095947ea818625d3
LAPTOP003 <xkeleter3@gmail.com>
2026-09-07 10:08:14 +0200

    feat: move DualRangeSlider to shared utils and update import paths

--- scripts/library/build/setFilter.ts
+++ scripts/library/build/setFilter.ts
@@ -5,7 +5,7 @@ import { HermidataModel } from "../../shared/utils/HermidataSelector";
 import { getElement } from "../../shared/utils/Selection";
 import type { Controller } from "../controller";
 import { Sort } from "./filter";
-import { DualRangeSlider } from "./starRating";
+import { DualRangeSlider } from "../../shared/utils/starRating";
 
 export class filter extends Sort {
 
--- scripts/library/controller.ts
+++ scripts/library/controller.ts
@@ -4,7 +4,7 @@ import { feed } from "./build/feed";
 import { filter } from "./build/setFilter";
 import { FilterLogic } from "./build/filterLogic";
 import { HermidataMerge } from "./build/merge";
-import type { DualRangeSlider } from "./build/starRating";
+import type { DualRangeSlider } from "../shared/utils/starRating";
 
 export class Controller {
     
