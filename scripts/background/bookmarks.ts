27af3648cec62cbb5f532ba3969bd787b6ee5bbf
LAPTOP003 <xkeleter3@gmail.com>
2026-09-10 13:55:55 +0200

    feat: enhance bookmark icon updates with dynamic color handling

--- scripts/background/bookmarks.ts
+++ scripts/background/bookmarks.ts
@@ -251,7 +251,6 @@ export function getBookmarkChildren(parentId = "2"): Promise<chrome.bookmarks.Bo
         });
     });
 }
-
 async function getCurrentTab(): Promise<chrome.tabs.Tab> {
     const [currentTab] = await ext.tabs.query({ active: true, currentWindow: true });
     return currentTab
