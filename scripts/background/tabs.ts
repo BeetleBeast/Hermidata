869c5e0f742fb2fd478375e01d75b5d1c35f2cfc
BeetleBeast <78571657+BeetleBeast@users.noreply.github.com>
2026-09-11 19:19:09 +0200

    fix: remove duplicate import of setDynamicIcon in tabs.ts

--- scripts/background/tabs.ts
+++ scripts/background/tabs.ts
@@ -3,7 +3,6 @@ import { getAllHermidata } from "../shared/db/Storage"
 import { updateCurrentBookmarkAndIcon } from "./bookmarks";
 import { allHermidataCashed, currentBookmark, currentTab, setState } from "./state";
 import { setDynamicIcon } from "../shared/utils/StringOutput";
-import { setDynamicIcon } from "../shared/utils/StringOutput";
 
 export function initTabs() {
     ext.tabs.onActivated.addListener(() => updateCurrentBookmarkAndIcon())
